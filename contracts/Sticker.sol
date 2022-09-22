d525c09418d0911ae4704fe24ec6d2fae9ad0771
ramsey.dao.dmtp <ramsey.dao.dmtp@highphen.com>
2022-09-22 15:58:21 +0700

    Update to ERC721A

--- contracts/Sticker.sol
+++ contracts/Sticker.sol
@@ -1,54 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./extensions/ERC721URIStorage.sol";
 
-contract Sticker is ERC721, ERC721URIStorage, AccessControl {
-    using Counters for Counters.Counter;
-
+contract Sticker is ERC721URIStorage, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Sticker", "STK") {
+    constructor() ERC721A("DMTP Sticker", "DMTPSTK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri)
-        public
-        onlyRole(MINTER_ROLE)
-    {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+    function mint(
+        address to,
+        uint256 quantity,
+        string[] memory uris
+    ) external onlyRole(MINTER_ROLE) {
+        require(
+            quantity == uris.length,
+            "Sticker: quantity and uris length mismatch"
+        );
+        uint256 startTokenId = _nextTokenId();
+        _mint(to, quantity);
+        for (uint256 i = 0; i < quantity; i++) {
+            _setTokenURI(startTokenId + i, uris[i]);
+        }
     }
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, AccessControl)
+        override(ERC721A, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
