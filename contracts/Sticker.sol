25e8d0c9af4fe3b790218d6f976f7ec123d1e461
ramsey.dao.dmtp <ramsey.dao.dmtp@highphen.com>
2022-10-11 11:10:03 +0700

    update dynamic id sticker

--- contracts/Sticker.sol
+++ contracts/Sticker.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Sticker is ERC1155, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
+    uint256 private _currentTokenID = 0;
     mapping(uint256 => string) private _tokenURIs;
 
     constructor() ERC1155("") {
@@ -16,22 +16,25 @@ contract Sticker is ERC1155, AccessControl {
 
     function mint(
         address account,
-        uint256 id,
         uint256 amount,
         string memory _uri
     ) public onlyRole(MINTER_ROLE) {
+        uint256 id = ++_currentTokenID;
         _tokenURIs[id] = _uri;
         _mint(account, id, amount, "");
     }
 
     function mintBatch(
         address to,
-        uint256[] memory ids,
         uint256[] memory amounts,
         string[] memory uris
     ) public onlyRole(MINTER_ROLE) {
-        for (uint256 i = 0; i < ids.length; i++) {
-            _tokenURIs[ids[i]] = uris[i];
+        require(amounts.length == uris.length, "Sticker: INVALID_INPUT_LENGTH");
+        uint256[] memory ids = new uint256[](amounts.length);
+        for (uint256 i = 0; i < uris.length; i++) {
+            uint256 id = ++_currentTokenID;
+            ids[i] = id;
+            _tokenURIs[id] = uris[i];
         }
         _mintBatch(to, ids, amounts, "");
     }
