9920c9203ea167385b84aa295818eefe64542f38
aqrare <takumitakumi0310@gmail.com>
2022-12-16 15:56:54 +0900

    fix: delete unnecessary code

--- contracts/interfaces/IDMTPMessages.sol
+++ contracts/interfaces/IDMTPMessages.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 interface IDMTPMessages {
     function sendMessages(address _from, address _to, string memory _message) external;
